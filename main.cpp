e760718fcc65dcbcffe28bd7fa798ff44fcc6f91
Hans Kirchner <hans-kirchner@gmx.de>
2016-08-29 02:03:28 +0200

    Change to Win32 Application (no console window)

--- main.cpp
+++ main.cpp
@@ -2,9 +2,13 @@
 #include <stdio.h>
 #include <string>
 #include <iostream>
+#include "Windows.h"
 #include "Shlobj.h"
 
-int main(int argc, char *argv[]) {
+// http://stackoverflow.com/questions/8945018/hiding-the-black-window-in-c
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
+                   LPTSTR lpCmdLine, int nCmdShow) {
+
     std::vector<AudioDevice> list;
     AudioDevice defaultDev;
 
@@ -24,56 +28,6 @@ int main(int argc, char *argv[]) {
 
     int nextIndex = (defaultIndex + 1) % list.size();
     AudioController::setDefault(list[nextIndex]);
-
-    return 0;
-}
-
-// EndPointController.exe [NewDefaultDeviceID]
-int unused_main(int argc, char *argv[])
-{
-    for (int i = 1; i < argc; i++)
-    {
-        if (strcmp(argv[i], "--help") == 0)
-        {
-            printf("Lists active audio end-point playback devices or sets default audio end-point\n");
-            printf("playback device.\n\n");
-            printf("USAGE\n");
-            printf("  EndPointController.exe [-a] [-f format_str]  Lists audio end-point playback\n");
-            printf("                                               devices that are enabled.\n");
-            printf("  EndPointController.exe device_index          Sets the default playvack device\n");
-            printf("                                               with the given index.\n");
-            printf("\n");
-            printf("OPTIONS\n");
-            printf("  -a             Display all devices, rather than just active devices.\n");
-            printf("  -f format_str  Outputs the details of each device using the given format\n");
-            printf("                 string. If this parameter is ommitted the format string\n");
-            printf("                 Parameters that are passed to the 'printf' function are\n");
-            printf("                 ordered as follows:\n");
-            printf("                   - Device index (int)\n");
-            printf("                   - Device friendly name (wstring)\n");
-            printf("                   - Device state (int)\n");
-            printf("				     - Device default? (1 for true 0 for false as int)\n");
-            printf("                   - Device description (wstring)\n");
-            printf("                   - Device interface friendly name (wstring)\n");
-            printf("                   - Device ID (wstring)\n");
-            exit(0);
-        }
-        else if (strcmp(argv[i], "-a") == 0)
-        {
-            continue;
-        }
-        else if (strcmp(argv[i], "-f") == 0)
-        {
-            if ((argc - i) >= 2) {
-                continue;
-            }
-            else
-            {
-                printf("Missing format string");
-                exit(1);
-            }
-        }
-    }
 
     return 0;
 }
